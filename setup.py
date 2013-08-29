1eea9c6829286e5239a585d773b233442e5b1520
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-08-28 20:57:31 -0400

    fix setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyadif',
+      version='0.1',
+      description='Python ADIF API',
+      author='Jeff Laughlin',
+      author_email='jeff@jefflaughlinconsulting.com',
+      url='https://github.com/n1ywb/pyadif',
+      py_modules = ['adif'],
+     )
+
